289a3454580067c9871d7892bfb37a90303705c7
Nicholas <nicholaskohlbecker@gmail.com>
2024-09-12 07:37:54 -0400

    Hello world need to find a simulator

---
+++ T01_HelloWorldTb.vhd
@@ -0,0 +1,18 @@
+entity T01_HelloWorldTb is 
+end entity;
+
+architecture sim of T01_HelloWorldTb is
+begin
+		
+	process is
+	begin
+		
+		report "Hello World!";
+		wait;
+	
+	end process;
+
+
+
+end architecture;
+
